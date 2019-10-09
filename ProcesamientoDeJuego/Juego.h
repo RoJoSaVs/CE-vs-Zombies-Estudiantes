5fe2d2f5e85147935cc3d7c9b8f0f6c83f8f70b3
toms99 <tomas.seguram@gmail.com>
2019-10-08 20:33:44 -0600

    Clase Objeto de juego, permite crear varios objetos de juego que heredan de el.

---
+++ ProcesamientoDeJuego/Juego.h
@@ -0,0 +1,13 @@
+#ifndef CE_VS_ZOMBIES_ESTUDIANTES_JUEGO_H
+#define CE_VS_ZOMBIES_ESTUDIANTES_JUEGO_H
+
+
+class Juego {
+public:
+    void run();
+
+
+};
+
+
+#endif //CE_VS_ZOMBIES_ESTUDIANTES_JUEGO_H
