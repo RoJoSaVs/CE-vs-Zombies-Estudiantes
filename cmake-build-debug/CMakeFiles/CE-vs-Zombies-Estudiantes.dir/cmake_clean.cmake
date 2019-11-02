1fae4687a7de73f900903ffb816635d754993eee
toms99 <tomas.seguram@gmail.com>
2019-11-02 01:04:35 -0600

    merge with master

---
+++ cmake-build-debug/CMakeFiles/CE-vs-Zombies-Estudiantes.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CE-vs-Zombies-Estudiantes_autogen"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/CE-vs-Zombies-Estudiantes_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/main.cpp.o"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/ObjetosDeJuego/ObjetoDeJuego.cpp.o"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/ObjetosDeJuego/ID.cpp.o"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/GUI/Window.cpp.o"
+  "CMakeFiles/CE-vs-Zombies-Estudiantes.dir/GUI/gameWindow.cpp.o"
+  "CE-vs-Zombies-Estudiantes.pdb"
+  "CE-vs-Zombies-Estudiantes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CE-vs-Zombies-Estudiantes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CE-vs-Zombies-Estudiantes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CE-vs-Zombies-Estudiantes.
+# This may be replaced when dependencies are built.
